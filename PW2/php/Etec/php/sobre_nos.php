4d085ad686aafd9c9356bb874a5eb8076b1fa0e4
Isaac Dantas Nogueira <dantasnogueiraisaac663@gmail.com>
2026-04-09 10:58:46 -0300

    Add files via upload

---
+++ PW2/php/Etec/php/sobre_nos.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Etec - Sobre Nós</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/sobre_nos.css">
+    <link rel="icon" href="../img/cps-favicon.png">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <a href="index.php"><img src="../img/cps-logo.png" alt="Logo CPS"></a>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="../php/index.php">Início</a></li>
+                <li><a href="../php/cursos.php">Cursos</a></li>
+                <li><a href="../php/contato.php">Contato</a></li>
+                <li><a href="../php/sobre_nos.php" class="ativa">Sobre nós</a></li>
+                <li><a href="../php/vestibulinho.php">Vestibulinho</a></li>
+            </ul>
+        </nav>
+    </header>
+    <div class="sobre-container">
+        <h1>Conheça a Etec da Zona Leste</h1>
+    <div class="sobre-bloco">
+        <h3>Excelência em Educação Técnica na Região da Zona Leste de São Paulo</h3>
+        <p>
+            Localizada estrategicamente na Avenida Águia de Haia, na Zona Leste de São Paulo, a Etec Zona Leste se destaca como um 
+            importante polo educacional na região. Sua história remonta ao compromisso com a excelência educacional e o desenvolvimento 
+            profissional dos estudantes. 
+            Fundada em 2008, a Etec Zona Leste rapidamente se estabeleceu como uma instituição de referência no ensino técnico e tecnológico. 
+            Desde o início, sua missão foi proporcionar oportunidades de aprendizado que preparassem os alunos para os desafios do mercado de 
+            trabalho, além de incentivá-los a buscar a excelência acadêmica. 
+            Ao longo dos anos, a escola tem evoluído e se adaptado às demandas da sociedade e do mercado, ampliando sua oferta de cursos e 
+            modernizando suas instalações. Com uma equipe dedicada de professores e funcionários, a Etec Zona Leste oferece uma variedade de 
+            cursos técnicos em áreas como informática, administração, eletrônica, entre outros, proporcionando aos alunos uma formação sólida e atualizada. 
+            Além da excelência acadêmica, a Etec Zona Leste também se destaca por suas atividades extracurriculares, que incluem projetos de pesquisa, 
+            competições acadêmicas, eventos culturais e esportivos. Essas atividades complementam o currículo escolar, promovendo o desenvolvimento integral 
+            dos estudantes e estimulando o trabalho em equipe, a liderança e a criatividade.
+        </p>
+    </div>
+    <div class="sobre-bloco">
+        <h3>O que a ETEC pode fazer pelo seu FUTURO ainda HOJE?</h3>
+        <p>
+            Sabemos que às vezes pensar no futuro pode parecer assustador. Traçamos carreiras e planos profissionais a todo momento em nossos pensamentos, 
+            numa busca constante pelo caminho que nos trará o melhor. Mas saiba que a ETEC Zona Leste conhece os seus anseios e, está aqui, disposta a te 
+            apresentar um novo caminho! O futuro não precisa ser assustador. Suas habilidades técnicas e profissionais podem ser desenvolvidas enquanto você 
+            aprende com a gente. Em nossa instituição, nós valorizamos os pequenos passos. Dê o seu primeiro passo hoje em direção ao futuro. Faça parte da ETEC!
+        </p>
+    <a href="../php/vestibulinho.php" class="botao-vestibulinho">Saiba mais sobre o Vestibulinho</a>
+    </div>
+    </div>
+    
+    <footer>
+        <div class="footer-container">
+            <div class="footer-col">
+                <img src="../img/cps-logo.png" alt="Logo CPS" class="footer-logo">
+                <p>Formação Técnica Gratuita e de Qualidade</p>
+            </div>
+            <div class="footer-col">
+                <h4>Links Rápidos</h4>
+                <ul>
+                    <div>
+                        <li><a href="../php/index.php">Início</a></li>
+                        <li><a href="../php/cursos.php">Cursos</a></li>
+                        <li><a href="../php/vestibulinho.php">Vestibulinho</a></li>
+                    </div>
+                    <div>
+                        <li><a href="../php/noticias.php">Notícias</a></li>
+                        <li><a href="../php/sobre_nos.php">Sobre nós</a></li>
+                        <li><a href="../php/contato.php">Contato</a></li>
+                    </div>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h4>Contato</h4>
+                <p>📍 Rua Conselheiro Carrão, 900 - Vila Carrão, São Paulo</p>
+                <p>📞 (11) 2045-4000</p>
+                <p>✉️ eteczonaleste@cps.sp.gov.br</p>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <p>© 2024 Etec da Zona Leste — Todos os direitos reservados</p>
+        </div>
+    </footer>
+</body>
+</html>
