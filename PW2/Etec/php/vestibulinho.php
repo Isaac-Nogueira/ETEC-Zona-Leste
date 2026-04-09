a2eaf2ea9f01add8a04a6fd21afd50485e438572
Isaac Dantas Nogueira <dantasnogueiraisaac663@gmail.com>
2026-04-09 06:30:26 -0300

    Add files via upload

---
+++ PW2/Etec/php/vestibulinho.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Etec - Vestibulinho</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/vestibulinho.css">
+    <link rel="icon" href="../img/cps-favicon.png">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <a href="index.php"><img src="../img/cps-logo.png" alt="Logo CPS"></a>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="../php/index.php">Início</a></li>
+                <li><a href="../php/cursos.php">Cursos</a></li>
+                <li><a href="../php/contato.php">Contato</a></li>
+                <li><a href="../php/sobre_nos.php">Sobre nós</a></li>
+                <li><a href="../php/vestibulinho.php" class="ativa">Vestibulinho</a></li>
+            </ul>
+        </nav>
+    </header>
+    <div class="banner">
+        <img src="../img/vest.png" alt="Vestibulunho">
+        <div class="banner-texto">
+            <h1>Vestibulinho</h1>
+            <p>Seu caminho para o ensino técnico gratuito começa aqui</p>
+        </div>
+    </div>
+    <div class="vest">
+        <h2>O Vestibulhinho das Escolas Técnicas Estaduais Ocorrem de Forma Semestral</h2>
+        <p>
+            O processo de vestibulinho na ETEC, segue um padrão definido pelo Centro Paula Souza. Primeiramente, é divulgado um edital contendo todas as informações 
+            essenciais, como datas, locais de prova, cursos oferecidos, requisitos de inscrição e documentação necessária. 
+            Os candidatos devem então se inscrever dentro do prazo estipulado, geralmente de forma online através do site oficial do vestibulinho da ETEC, e pagar uma 
+            taxa de inscrição conforme especificado no edital.
+        </p>
+        <a href="../php/contato.php">Inscreva-se</a>
+    </div>
+    <div class="prova">
+        <h2>A Prova</h2>
+        <p>
+            A prova do vestibulinho consiste em 50 questões de múltipla escolha, abrangendo várias áreas do conhecimento, incluindo Língua Portuguesa, Matemática, 
+            Ciências e questões específicas relacionadas ao curso técnico escolhido. Após a correção das provas, os resultados são divulgados dentro do prazo estabelecido, 
+            permitindo que os candidatos verifiquem sua pontuação e classificação. Os aprovados devem então proceder com a matrícula dentro do prazo estabelecido, seguindo 
+            as orientações fornecidas pela ETEC, o que inclui a apresentação de documentos e o preenchimento de formulários específicos. 
+            É fundamental que você, candidato, esteja atento à modalidade de curso que escolheu, pois o Centro Paula Souza não assume responsabilidade por inscrições em cursos 
+            incorretos ou modalidades não desejadas. Certifique-se de selecionar cuidadosamente o curso e a modalidade desejados durante o processo de inscrição, garantindo 
+            assim uma participação adequada e de acordo com suas preferências e objetivos educacionais. 
+        </p>
+    </div>
+    <footer>
+        <div class="footer-container">
+            <div class="footer-col">
+                <img src="../img/cps-logo.png" alt="Logo CPS" class="footer-logo">
+                <p>Formação Técnica Gratuita e de Qualidade</p>
+            </div>
+            <div class="footer-col">
+                <h4>Links Rápidos</h4>
+                <ul>
+                    <div>
+                        <li><a href="../php/index.php">Início</a></li>
+                        <li><a href="../php/cursos.php">Cursos</a></li>
+                        <li><a href="../php/vestibulinho.php">Vestibulinho</a></li>
+                    </div>
+                    <div>
+                        <li><a href="../php/noticias.php">Notícias</a></li>
+                        <li><a href="../php/sobre_nos.php">Sobre nós</a></li>
+                        <li><a href="../php/contato.php">Contato</a></li>
+                    </div>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h4>Contato</h4>
+                <p>📍 Rua Conselheiro Carrão, 900 - Vila Carrão, São Paulo</p>
+                <p>📞 (11) 2045-4000</p>
+                <p>✉️ eteczonaleste@cps.sp.gov.br</p>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <p>© 2024 Etec da Zona Leste — Todos os direitos reservados</p>
+        </div>
+    </footer>
+</body>
+</html>
